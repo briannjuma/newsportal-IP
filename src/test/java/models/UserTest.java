1cdc8f919189e3da079e9ff0f7598e0c895a7e37
jumaBrian <njengabrianj@gmail.com>
2022-06-17 14:53:29 +0300

    chore: Write tests for models

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -1,4 +1,34 @@
 package models;
 
+import dao.DatabaseRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@SuppressWarnings("ConstantConditions")
+
 public class UserTest {
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
+
+    private User newUser(){
+        return new User("Kanye West","Manager","Artist","Media");
+    }
+
+    @Test
+    public void initializeCorrectly(){
+        User user = newUser();
+        assertTrue(user instanceof User);
+    }
+
+    @Test
+    public void getFunctionsWorkWell(){
+        User user = newUser();
+        assertEquals(user.getName(),newUser().getName());
+        assertEquals(user.getPosition(),newUser().getPosition());
+        assertEquals(user.getRole(),newUser().getRole());
+        assertEquals(user.getDepartment(),newUser().getDepartment());
+    }
 }
