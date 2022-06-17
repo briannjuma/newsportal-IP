0d5925b7000b9058004a7d59a11f1da61104090e
jumaBrian <njengabrianj@gmail.com>
2022-06-17 15:20:54 +0300

    chore: Add Json configuration

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,22 @@
+import static spark.Spark.*;
 
+import com.google.gson.Gson;
+import dao.SQL2oSitemapDao;
 import models.*;
+import dao.Sql2oNewsDao;
+import dao.Sql2oDeptDao;
+import dao.Sql2oUserDao;
 
 public class App {
     public static void main(String[] args) {
 
+
+
+        get("/sitemap","application/json",(request, response) ->{
+            return gson.toJson(sitemapDao.allPaths());
+        });
+
+        //filter
+        after((req, res) -> res.type("application/json"));
     }
 }
