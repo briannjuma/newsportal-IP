92b46a4977b7426dd3b28e634d81d57bbc89872f
jumaBrian <njengabrianj@gmail.com>
2022-06-17 13:48:02 +0300

    chore: Include database set-up instruction

---
+++ create.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE news_portal;
+
+CREATE TABLE users (
+    id serial primary key,
+    name varchar,
+    position VARCHAR,
+    role varchar,
+    department varchar
+    );
+
+CREATE TABLE news (
+    id serial primary key,
+    title varchar,
+    description varchar,
+    type varchar,
+    author varchar
+    );
+
+CREATE TABLE departments (
+    id serial primary key,
+    title varchar,
+    description varchar,
+    totalemployees int,
+    );
+
+CREATE TABLE departments_users (
+     id serial primary key,
+     deptid int,
+     userid int,
+     );
+
+CREATE TABLE departments_news (
+     id serial primary key,
+     deptid int,
+     newsid int
+     userid int,
+     );
+
+CREATE DATABASE news_portal_test WITH TEMPLATE news_portal;
